159076156dad9aadb0ec0f9c4d01bf18ecd1218e
DacharamGaneshReddy <131764210+DacharamGaneshReddy@users.noreply.github.com>
2026-02-03 13:50:07 +0530

    first commit

---
+++ Passenger-info/src/SecurityGAte.java
@@ -0,0 +1,19 @@
+public class SecurityGAte{
+    public static void main (String[] args){
+        // 1. variable declaration and initialization
+        String passengerName = "Ganesh Reddy";
+        int passengerAge = 22;
+        boolean BoardingPass = true;
+        double Weight = 18.5;
+        char gender = 'M';
+
+        // processing
+        System.out.println("processing passenger:" + passengerName );
+        System.out.println("Age of passenger:" + passengerAge  );
+        System.out.println("passenger boading pass:" + BoardingPass);
+        System.out.println("Weight of passenger:" + Weight  );
+        System.out.println("Gender of passenger:" + gender  );
+
+    }
+}
+
